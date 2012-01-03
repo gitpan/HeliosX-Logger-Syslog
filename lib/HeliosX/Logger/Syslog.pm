b9d174f240ecc2dde8cc7cf6a21c7500f13b25b2
Andrew Johnson <lajandy@cpan.org>
2012-01-03 06:27:27 +0000

    Import of LAJANDY/HeliosX-Logger-Syslog-0.04_0111 from CPAN.
    gitpan-cpan-distribution: HeliosX-Logger-Syslog
    gitpan-cpan-version:      0.04_0111
    gitpan-cpan-path:         LAJANDY/HeliosX-Logger-Syslog-0.04_0111.tar.gz
    gitpan-cpan-author:       LAJANDY
    gitpan-cpan-maturity:     developer

--- lib/HeliosX/Logger/Syslog.pm
+++ lib/HeliosX/Logger/Syslog.pm
@@ -10,7 +10,7 @@ use Sys::Syslog;
 use Helios::LogEntry::Levels qw(:all);
 use Helios::Error::LoggingError;
 
-our $VERSION = '0.03_4931';
+our $VERSION = '0.04_0111';
 
 =head1 NAME
 
@@ -21,8 +21,10 @@ HeliosX::Logger::Syslog - Helios::Logger subclass implementing logging to syslog
  # in helios.ini
  loggers=HeliosX::Logger::Syslog
  syslog_facility=user
- # can use other options as necessary
+ # (optional) you can use other options as necessary
  syslog_options=nofatal,pid 
+ # (optional) you can set the logmask by using integer values
+ syslog_logmask=127
 
 =head1 DESCRIPTION
 
@@ -35,14 +37,45 @@ Config options:
 
 =over 4
 
-=item syslog_facility [REQUIRED]
+=item syslog_facility
 
-The syslogd facility to log messages to. 
+The syslogd facility to which to log messages.  If not specified, it will 
+default to 'user'.
 
 =item syslog_options
 
-A comma-delimited list of syslog options.  This will be passed as the second parameter of 
-openlog().  See the L<Sys::Syslog> manpage for more details.
+A comma-delimited list of syslog options.  This will be passed as the second 
+parameter of openlog().  See the L<Sys::Syslog> manpage for more details.
+
+=item syslog_logmask
+
+Allows you choose which log priorities you want to syslogd to actually log.  
+This is like the Helios internal log_priority_threshold, but more capable as 
+you can pick and choose which priorities you want, rather than just a range.
+
+Syslogd defines the mask values for priorities as:
+
+ 1   = LOG_EMERG
+ 2   = LOG_ALERT
+ 4   = LOG_CRIT
+ 8   = LOG_ERR
+ 16  = LOG_WARNING
+ 32  = LOG_NOTICE
+ 64  = LOG_INFO
+ 128 = LOG_DEBUG
+ 
+So, for example, if you wanted to log everything except LOG_DEBUG messages, 
+putting:
+
+syslog_logmask=127
+
+in your helios.ini or Ctrl Panel will cause syslogd to filter out messages of 
+LOG_DEBUG priority.  In addition, to only log LOG_ERR and LOG_WARNING messages:
+
+syslog_logmask=24
+
+will filter out any messages not of LOG_ERR or LOG_WARNING priority 
+(8 + 16 = 24).
 
 =back
 
@@ -50,20 +83,11 @@ openlog().  See the L<Sys::Syslog> manpage for more details.
 
 =head2 init()
 
-The init() method verifies that the syslog_facility configuration parameter is set in helios.ini.  
-Without it, HeliosX::Logger::Syslog (and Sys::Syslog) will not function properly.
+The init() method is empty.
 
 =cut
 
-sub init {
-    my $self = shift;
-    my $config = $self->getConfig();
-
-    unless ( defined($config->{syslog_facility}) ) {
-        throw Helios::Error::LoggingError("CONFIGURATION ERROR: syslog_facility not defined"); 
-    }
-    return 1;
-}
+sub init { }
 
 
 =head2 logMsg($job, $priority_level, $message)
@@ -78,10 +102,47 @@ sub logMsg {
     my $job = shift;
     my $priority = shift;
     my $msg = shift;
-    
-    openlog($self->getJobType(), $self->getConfig()->{syslog_options}, $self->getConfig()->{syslog_facility});
-    syslog($priority, $msg);
+	my $config = $self->getConfig();
+	my $facility;
+	my $options;
+	
+	# default to facility 'user'
+	if ( !defined($config->{syslog_facility}) ) {
+		$facility = 'user';
+	} else {
+		$facility = $config->{syslog_facility};
+	}
+	# use options if specified
+	if ( defined($config->{syslog_options}) ) {
+		$options = $config->{syslog_options};
+	}
+
+    openlog($self->getJobType(), $options, $facility);
+    if ( defined($config->{syslog_logmask}) ) {
+    	setlogmask($config->{syslog_logmask});
+    }
+    syslog($priority, $self->assembleMsg($job, $priority, $msg));
     closelog();
+}
+
+
+=head2 assembleMsg($job, $priority_level, $msg)
+
+Given the information passed to logMsg(), assembleMsg() returns the actual text 
+string to be logged to syslogd.  Separating this step into its own method 
+allows you to easily override the default message format if you so 
+choose.  Simply subclass HeliosX::Logger::Syslog and override assembleMsg() 
+with your own message formatting method.
+
+=cut
+
+sub assembleMsg {
+	my ($self, $job, $priority, $msg) = @_;
+    if ( defined($job) ) { 
+    	return 'Job '.$job->getJobid().': '.$msg;
+    } else {
+    	return $msg;
+    }
 }
 
 
